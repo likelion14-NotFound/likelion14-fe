6a9f3a6b715722ad8dea4e229a9548005978d076
kimseohyeon1533 <kimseohyeon1533@gmail.com>
2026-06-19 17:21:24 +0900

    Subscribe 구현

--- src/pages/Subscribe/Subscribe.jsx
+++ src/pages/Subscribe/Subscribe.jsx
@@ -1,9 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 
-import HeroSection from "../../components/HeroSection/HeroSection.jsx";
-import DotDivider from "../../components/DotDivider/DotDivider.jsx";
-import MascotCard from "../../components/common/MascotCard/MascotCard.jsx";
-import CTAButton from "../../components/CTAButton/CTAButton.jsx";
+import TopSection from "../../components/common/topSection/TopSection.jsx";
 
 import letterImg from "../../assets/images/letter.svg";
 import humanImg from "../../assets/images/human.svg";
@@ -13,13 +11,12 @@ import "./Subscribe.css";
 function Subscribe() {
   return (
     <main className="subscribe-page">
-      <HeroSection
-        eyebrow="매주 만나는"
-        highlight="멋쟁이사자"
-        title="뉴스레터."
+      <TopSection
+        firstText="매주 만나는"
+        pinkText="멋쟁이사자"
+        normalText="뉴스레터."
         image={letterImg}
-        imageAlt="뉴스레터 봉투 이미지"
-        showBack={true}
+        alt="뉴스레터 봉투 이미지"
       />
 
       <section className="subscribe-question-section">
@@ -39,9 +36,15 @@ function Subscribe() {
           <p>우선, 비슷한 디자인과 기능을 하는 최소 단위를 찾는 것이 중요해요!</p>
           <p>팀원끼리 컴포넌트를 분담하고 작업하는 걸 추천합니다.</p>
           <p>당연히, 팀워크 요소도 빠질 수 없겠죠?</p>
-          <p>브랜치나 함수 명명 규칙, 협업 전략에 대해 고민하는 시간을 가져야 합니다.</p>
+          <p>
+            브랜치나 함수 명명 규칙, 협업 전략에 대해 고민하는 시간을 가져야
+            합니다.
+          </p>
           <p>마일스톤을 철저히 지키고 의견 공유를 하는 것도 중요한데요,</p>
-          <p>특히, 퍼블리싱과 API 연동을 하는 팀이 착착할 수 있어서 부지런해야 합니다.</p>
+          <p>
+            특히, 퍼블리싱과 API 연동을 하는 팀이 착착할 수 있어서 부지런해야
+            합니다.
+          </p>
         </div>
       </section>
 
@@ -50,7 +53,9 @@ function Subscribe() {
 
         <h3 className="section-title">이전 세션 자료들을 복습해요!</h3>
 
-        <MascotCard image={humanImg} alt="멋쟁이사자 마스코트" />
+        <div className="mascot-card">
+          <img src={humanImg} alt="멋쟁이사자 마스코트" />
+        </div>
 
         <div className="session-card-list">
           <div className="session-card">1주차 세션</div>
@@ -62,10 +67,37 @@ function Subscribe() {
           </button>
         </div>
 
-        <CTAButton>가입하기</CTAButton>
+        <button type="button" className="subscribe-join-button">
+          가입하기
+        </button>
       </section>
     </main>
   );
 }
+
+function DotDivider() {
+  return (
+    <DotDividerBox>
+      <span />
+      <span />
+      <span />
+    </DotDividerBox>
+  );
+}
+
+const DotDividerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 7px;
+  margin: 14px 0 26px;
+
+  span {
+    width: 5px;
+    height: 5px;
+    background-color: #4b22f4;
+    border-radius: 50%;
+  }
+`;
 
 export default Subscribe;
