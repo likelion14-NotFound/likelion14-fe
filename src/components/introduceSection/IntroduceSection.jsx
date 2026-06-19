78afc35d2dfbad20680def24cdf94c49bc1229b4
minsol-orang <minsol030507@naver.com>
2026-06-19 21:10:14 +0900

    ✨ Feat: IntroduceSection 자기소개 박스 구현

--- src/components/introduceSection/IntroduceSection.jsx
+++ src/components/introduceSection/IntroduceSection.jsx
@@ -33,7 +33,6 @@ const DotBox = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 28px;
-
 `;
 
 const Dot = styled.div`
@@ -48,6 +47,29 @@ const Title = styled.p`
   padding-top: 51px;
 `;
 
+const IntroduceBox = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+
+  height: 349px;
+  width: 1051px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 20px;
+  background: #fdf9ff;
+`;
+
+const Introduce = styled.div`
+  color: #b6b6b6;
+
+  font-family: "Pretendard Variable";
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
 export default function IntroduceSection() {
   return (
     <SectionContainer>
@@ -55,13 +77,20 @@ export default function IntroduceSection() {
         <QuestionText>Q1</QuestionText>
 
         <DotBox>
-    <Dot>.</Dot>
-    <Dot>.</Dot>
-    <Dot>.</Dot>
-  </DotBox>
+          <Dot>.</Dot>
+          <Dot>.</Dot>
+          <Dot>.</Dot>
+        </DotBox>
 
         <Title>프론트엔드에 대하여</Title>
       </QuestionBox>
+      <IntroduceBox>
+        <Introduce>
+          문제를 단순히 해결하는 데 그치지 않고, 더 나은 방향을 고민하는 개발자
+          김서현
+        </Introduce>
+        <Introduce>느려도 멈추지 않는 개발자 김민솔 입니다.</Introduce>
+      </IntroduceBox>
     </SectionContainer>
   );
 }
